4e790a0020c74a82dbad99cd653045cdcdad45d4
Nat Marn <112994197+HorrySheet11@users.noreply.github.com>
2025-09-03 09:47:25 +0800

    Add files via upload

---
+++ reusableFooter/index.jsx
@@ -0,0 +1,26 @@
+export const Footer = () => {
+  return (
+    <footer>
+    <ul>
+      <li>Link 1</li>
+      <li>Link 2</li>
+    </ul>
+    <ul>
+      <li>Link 3</li>
+      <li>Link 4</li>
+    </ul>
+    <ul>
+      <li>Link 5</li>
+      <li>Link 6</li>
+    </ul>
+    
+    <p>© 2025 Horry Sheet Dashboard. All Rights Reserved.</p>
+
+    <a href="#">🎮</a>
+    <a href="#">💻</a>
+    <a href="#">🏀</a>
+    
+    </footer>
+
+  )
+};
