e2ad5c932445062baef99b93fe51fac1ba7f73fa
Nat Marn <112994197+HorrySheet11@users.noreply.github.com>
2025-09-20 18:40:49 +0800

    Add files via upload

---
+++ reactAPIs/deleteAPI/axiosDeleteAPI.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+import React, { useState } from 'react';
+
+// Put request with axios
+
+const DeleteData= () => {
+
+  const handleDelete =  async () => {
+    try {
+      await axios.delete('https://jsonplaceholder.typicode.com/todos/1')
+      .then(res => console.log(res.data))
+      alert('Data deleted')
+    } catch (error) {
+      alert('Error deleting data.')
+    }
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        
+        <h2>Deleting API Data</h2>
+        <button
+          onClick={handleDelete}
+        >Delete Data
+          </button>
+
+      </header>
+    </div>
+  )
+};
+
+export default DeleteData;
