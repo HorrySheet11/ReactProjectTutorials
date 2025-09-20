499a503c27d08f212b4c689b6ad915ddf843024d
Nat Marn <112994197+HorrySheet11@users.noreply.github.com>
2025-09-20 10:30:03 +0800

    Add files via upload

--- reactAPIs/postApi/axiosPostAPI.js
+++ reactAPIs/postApi/axiosPostAPI.js
@@ -1,49 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
-import React from 'react';
+import React, { useState } from 'react';
 
 // Post request with axios
 
-export default class App extends React.Component {
-  state = {
-    name: '',
-    result: []
-  };
+const CreateData= () => {
+  const [newUser, setNewUser] = useState('');
+  const [isCustomer, setIsCustomer] = useState(false);
 
-  handleChange = (event) =>{
-    this.setState({name: event.target.value})
+  const handleCreate = async () =>{
+    try{
+      const user = {
+        name: newUser, 
+        customer: isCustomer
+      }
+
+      await axios.post('https://jsonplaceholder.typicode.com/todos',{user})
+      .then(res => console.log(res.data))
+      alert('Data created successfully :>')
+    }catch (error){
+      console.log('Error creating data', error)
+    }
   }
 
-  handleSubmit = (event) =>{
-    event.preventDefault();
-
-    const user = {
-      name: this.state.name
-    };
-
-    axios
-      .post(`https://jsonplaceholder.typicode.com/users`,{user})
-      .then((res) =>{
-        this.setState({result: res.data.user.name});
-      });
-  }
-
-
-  render(){  return (
+    return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Person Name: 
-            <input type='text' name='name' onChange={this.handleChange} />
-          </label>
-          <button type='submit'>Add</button>
-        </form>
-        <h4>Result: {this.state.result}</h4>
+        <h2>Create new Data: </h2>
+        <label>Name:   
+          <input 
+          type='text'
+          value={newUser}
+          onChange={(e)=> setNewUser(e.target.value)}
+        /></label>
+
+        <label>Customer? <select 
+          type='select'
+          value={isCustomer}
+          onChange={(e)=> setIsCustomer(e.target.value)}
+        >
+          <option value={true}>Yes</option>
+          <option value={false}>No</option>
+        </select>
+        </label>
+        <button onClick={handleCreate}>Create</button>
       </header>
     </div>
-  );}
+  )
 };
+
+export default CreateData;
